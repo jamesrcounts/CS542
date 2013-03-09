4194a3a3cfbb241e6cc56cffd901e3d783d2ef01
Jim Counts <count009@cougars.csusm.edu>
2013-03-09 12:14:59 -0800

    More Yak Shaving

--- HW6/hw6.cpp
+++ HW6/hw6.cpp
@@ -1,45 +1,15 @@
-/* hw5.cpp */
+/* hw6.cpp */
 
 #include <iostream>
-#include <vector>
-
-#include "Alien.h"
-#include "Animal.h"
-#include "Horse.h"
-#include "Snake.h"
 
 using namespace std;
-
-void MoveAnimals( vector<Animal *> animals )
-{
-    for ( vector<Animal *>::iterator it = animals.begin();
-            it != animals.end();
-            ++it )
-    {
-        Animal *a = *it;
-        a->Move();
-    }
-}
 
 int main( int argc, char const *argv[] )
 {
     cout << "Jim Counts" << endl;
     cout << "CS 542 - OO" << endl;
-    cout << "Homework 5" << endl << endl;
+    cout << "Homework 6" << endl << endl;
 
-    vector<Animal *> group;
-    group.push_back( new Snake() );
-    group.push_back( new Horse() );
-    group.push_back( new Alien() );
-
-    MoveAnimals( group );
-
-    for ( vector<Animal *>::iterator it = group.begin();
-            it != group.end();
-            ++it )
-    {
-        delete *it;
-    }
 
     return 0;
 }
--- HW6/test.cpp
+++ HW6/test.cpp
@@ -1,91 +1,9 @@
 /* test.cpp */
 
-#include <iostream>
 #include <igloo/igloo.h>
-#include <sstream>
-#include <string>
-
-#include "Animal.h"
-#include "Snake.h"
-#include "Horse.h"
-#include "Alien.h"
 
 using namespace std;
 using namespace igloo;
-
-Context( DescribeASnake )
-{
-    Spec( ItSlithers )
-    {
-        string expected( "Slithering... 5m.\n" );
-        stringstream spy;
-        Snake s;
-        s.UseSpy( spy );
-        s.Move();
-        Assert::That( spy.str(), Equals( expected ) );
-    }
-};
-
-Context( DescribeAHorse )
-{
-    Spec( ItGallops )
-    {
-        string expected( "Galloping... 45m.\n" );
-        stringstream spy;
-        Horse h;
-        h.UseSpy( spy );
-        h.Move();
-        Assert::That( spy.str(), Equals( expected ) );
-    }
-};
-
-Context( DescribeAnAlien )
-{
-    Spec( ItTeleports )
-    {
-        string expected( "Teleporting... 1000ly.\n" );
-        stringstream spy;
-        Alien a;
-        a.UseSpy( spy );
-        a.Move();
-        Assert::That( spy.str(), Equals( expected ) );
-    }
-};
-
-Context( DescribeAGroupOfAnimals )
-{
-    Spec( TheyCanMove )
-    {
-        string expected( "Slithering... 5m.\n" );
-        expected += "Galloping... 45m.\n";
-        expected += "Teleporting... 1000ly.\n";
-
-        vector<Animal *> group;
-        group.push_back( new Snake() );
-        group.push_back( new Horse() );
-        group.push_back( new Alien() );
-
-        stringstream spy;
-
-        for ( vector<Animal *>::iterator it = group.begin();
-                it != group.end();
-                ++it )
-        {
-            Animal *a = *it;
-            a->UseSpy( spy );
-            a->Move();
-        }
-
-        Assert::That( spy.str(), Equals( expected ) );
-
-        for ( vector<Animal *>::iterator it = group.begin();
-                it != group.end();
-                ++it )
-        {
-            delete *it;
-        }
-    }
-};
 
 int main( )
 {
