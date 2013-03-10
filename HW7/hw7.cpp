88ed13d1cb6ae6111194c190930e0b9817f98870
Jim Counts <count009@cougars.csusm.edu>
2013-03-10 16:19:35 -0700

    Yak shaving

---
+++ HW7/hw7.cpp
@@ -0,0 +1,17 @@
+/* hw7.cpp */
+
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char const *argv[] )
+{
+    cout << "Jim Counts" << endl;
+    cout << "CS 542 - OO" << endl;
+    cout << "Homework 7" << endl << endl;
+
+    return 0;
+}
+
+
+
---
+++ HW7/test.cpp
@@ -0,0 +1,11 @@
+/* test.cpp */
+
+#include <igloo/igloo.h>
+
+using namespace std;
+using namespace igloo;
+
+int main( )
+{
+    return TestRunner::RunAllTests();
+}
