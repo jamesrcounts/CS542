9358e4bd210a13ea0fda77b28a18fc6e79be9e22
Jim Counts <count009@cougars.csusm.edu>
2013-03-09 20:00:08 -0800

    Converted Array to template

--- HW6/hw6.cpp
+++ HW6/hw6.cpp
@@ -13,3 +13,35 @@ int main( int argc, char const *argv[] )
 
     return 0;
 }
+
+template
+< class T >
+void fillMatrix( const Matrix <T> & m )
+{
+    int i, j;
+
+    for ( i = 0; i < m.numRows(); i++ )
+    {
+        m[i][0] = T();
+    }
+
+    for ( j = 0; j < m.numCols(); j++ )
+    {
+        m[0][j] = T();
+    }
+
+    for ( i = 1; i < m.numRows(); i++ )
+        for ( j = 1; j < m.numCols(); j++ )
+        {
+            m[i][j] = T( i * j );
+        }
+}
+int main()
+{
+    Matrix < int > m( 10, 5 );
+    fillMatrix( m );
+    cout << m;
+    Matrix < double > M( 8, 10 );
+    fillMatrix( M );
+    cout << M;
+}
--- HW6/test.cpp
+++ HW6/test.cpp
@@ -1,9 +1,186 @@
 /* test.cpp */
 
 #include <igloo/igloo.h>
+#include <iomanip>
 
 using namespace std;
 using namespace igloo;
+
+/*
+template
+< class Element >
+class Matrix
+{
+private:
+   int rows, cols;
+ // define m as an Array of Array pointers using the template
+ // you defined above
+public:
+   Matrix( int newRows, int newCols )
+   : rows( newRows ), cols( newCols ), m( rows )
+   {
+     for (int i = 0; i < rows; i++ )
+       m[i] = new Array < Element >( cols );
+}
+int numRows()
+{
+ return rows;
+}
+int numCols()
+{
+ return cols;
+}
+Array < Element > & operator [] ( int row )
+{
+ return * m[row];
+}
+void print( ostream & out )
+{
+   // You can write this one too, but use the Array::operator<<
+}
+friend ostream & operator << ( ostream & out, Matrix & m )
+{
+ m.print( out );
+ return out;
+}
+};
+*/
+template <class Element>
+class Array
+{
+private:
+    int sz;
+    Element *buf;
+public:
+    Array( int size ) : sz( size ), buf( new Element[sz] ) {}
+
+    Array( Array &l ) : sz( l.sz ), buf( new Element[l.sz] )
+    {
+        for ( int i = 0; i < l.sz; i++ )
+        {
+            buf[i] = l.buf[i];
+        }
+    }
+
+    int length()
+    {
+        return sz;
+    }
+
+    void print( ostream &out )
+    {
+        for ( int i = 0; i < sz; i++ )
+        {
+            out << setw( 5 ) << buf[i];
+        }
+    }
+
+    Element &operator []( int i )
+    {
+        assert( 0 <= i && i < sz );
+        return buf[i];
+    }
+
+    friend ostream &operator << ( ostream &out, Array &a )
+    {
+        a.print( out );
+        return out;
+    }
+
+    friend ostream &operator << ( ostream &out, Array *ap )
+    {
+        ap->print( out );
+        return out;
+    }
+};
+
+void InitializeArray( Array<int> &a )
+{
+    for ( int i = 0; i < a.length(); ++i )
+    {
+        a[i] = 0;
+    }
+}
+
+Context( DescribeAnArray )
+{
+    Spec( ItIsATemplateClass )
+    {
+        Array<float> it( 3 );
+
+        for ( int i = 0; i < it.length(); ++i )
+        {
+            it[i] = i * 0.5f;
+        }
+
+        stringstream spy;
+        it.print( spy );
+        Assert::That( spy.str(), Equals( "    0  0.5    1" ) );
+    }
+
+    Spec( ItHasADefinedLength )
+    {
+        Array<int> it( 10 );
+        Assert::That( it.length(), Equals( 10 ) );
+    }
+
+    Spec( ItHasMutableCells )
+    {
+        Array<int> it( 10 );
+        it[5] = 99;
+        Assert::That( it[5], Equals( 99 ) );
+    }
+
+    Spec( ItIsPrintable )
+    {
+        string expected( "    0    0    0" );
+        stringstream spy;
+        Array<int> it( 3 );
+        InitializeArray( it );
+
+        it.print( spy );
+        Assert::That( spy.str(), Equals( expected ) );
+    }
+
+    Spec( ItCanBeExtractedByReference )
+    {
+        string expected( "    0    0    0" );
+        stringstream spy;
+        Array<int> it( 3 );
+        InitializeArray( it );
+
+        spy << it;
+        Assert::That( spy.str(), Equals( expected ) );
+    }
+
+    Spec( ItCanBeExtractedByPointer )
+    {
+        string expected( "    0    0    0" );
+        stringstream spy;
+        Array<int> *it = new Array<int>( 3 );
+        InitializeArray( *it );
+        spy << it;
+        Assert::That( spy.str(), Equals( expected ) );
+        delete it;
+    }
+
+    Spec( ItCanBeCopiedIntoAnotherArray )
+    {
+        string expected( "    1    2    3" );
+        Array<int> other( 3 );
+
+        for ( int i = 0; i < other.length(); ++i )
+        {
+            other[i] = i + 1;
+        }
+
+        Array<int> it( other );
+
+        stringstream spy;
+        it.print( spy );
+        Assert::That( spy.str(), Equals( expected ) );
+    }
+};
 
 int main( )
 {
