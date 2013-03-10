933f28e5a1d873c275baf3e60a1a82fb53527758
Jim Counts <count009@cougars.csusm.edu>
2013-03-09 20:14:16 -0800

    Moved Array to file

--- HW6/test.cpp
+++ HW6/test.cpp
@@ -2,6 +2,7 @@
 
 #include <igloo/igloo.h>
 #include <iomanip>
+#include "Array.h"
 
 using namespace std;
 using namespace igloo;
@@ -45,54 +46,7 @@ friend ostream & operator << ( ostream & out, Matrix & m )
 }
 };
 */
-template <class Element>
-class Array
-{
-private:
-    int sz;
-    Element *buf;
-public:
-    Array( int size ) : sz( size ), buf( new Element[sz] ) {}
 
-    Array( Array &l ) : sz( l.sz ), buf( new Element[l.sz] )
-    {
-        for ( int i = 0; i < l.sz; i++ )
-        {
-            buf[i] = l.buf[i];
-        }
-    }
-
-    int length()
-    {
-        return sz;
-    }
-
-    void print( ostream &out )
-    {
-        for ( int i = 0; i < sz; i++ )
-        {
-            out << setw( 5 ) << buf[i];
-        }
-    }
-
-    Element &operator []( int i )
-    {
-        assert( 0 <= i && i < sz );
-        return buf[i];
-    }
-
-    friend ostream &operator << ( ostream &out, Array &a )
-    {
-        a.print( out );
-        return out;
-    }
-
-    friend ostream &operator << ( ostream &out, Array *ap )
-    {
-        ap->print( out );
-        return out;
-    }
-};
 
 void InitializeArray( Array<int> &a )
 {
